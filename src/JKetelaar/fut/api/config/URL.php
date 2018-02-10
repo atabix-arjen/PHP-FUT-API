a1090f576cffbd488ae637331699da528b5cc754
Arjen Bos <arjen.bos@atabix.nl>
2018-02-10 18:13:17 +0100

    Update URL.php

--- src/JKetelaar/fut/api/config/URL.php
+++ src/JKetelaar/fut/api/config/URL.php
@@ -6,15 +6,15 @@
 namespace JKetelaar\fut\api\config;
 
 abstract class URL {
-    const REFERER = 'https://www.easports.com/iframe/fut17/?locale=en_US&baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Fde%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fde%2Ffifa%2Fultimate-team%2Fweb-app';
+    const REFERER = 'https://www.easports.com/fifa/ultimate-team/web-app/';
 
     // Login URLs
     const LOGIN_MAIN     = 'https://www.easports.com/fifa/ultimate-team/web-app';
     const LOGIN_NUCLEUS  = 'https://www.easports.com/iframe/fut17/?locale=en_US&baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Fde%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fde%2Ffifa%2Fultimate-team%2Fweb-app';
     const LOGIN_PERSONAS = 'https://www.easports.com/fifa/api/personas';
-    const LOGIN_SHARDS   = 'https://www.easports.com/iframe/fut17/p/ut/shards/v2';
-    const LOGIN_ACCOUNTS = 'https://www.easports.com/iframe/fut17/p/ut/game/fifa17/user/accountinfo?filterConsoleLogin=true&sku=FUT17WEB&returningUserGameYear=2016&_=';
-    const LOGIN_SESSION  = 'https://www.easports.com/iframe/fut17/p/ut/auth';
+    const LOGIN_SHARDS   = 'https://utas.mob.v4.fut.ea.com/ut/shards/v2';
+    const LOGIN_ACCOUNTS = 'https://utas.external.s2.fut.ea.com/ut/game/fifa18/user/accountinfo?filterConsoleLogin=true&sku=FUT17WEB&returningUserGameYear=2016&_=';
+    const LOGIN_SESSION  = 'https://utas.external.s2.fut.ea.com/ut/auth';
     const LOGIN_QUESTION = 'https://utas.external.s2.fut.ea.com/ut/game/fifa18/phishing/question?_=';
     const LOGIN_VALIDATE = 'https://utas.external.s2.fut.ea.com/ut/game/fifa18/phishing/validate?_=';
 
@@ -34,5 +34,5 @@ abstract class URL {
 
     // Players endpoints
     const PLAYERS_DATABASE = 'https://www.easports.com/fifa/ultimate-team/web-app/content/B1BA185F-AD7C-4128-8A64-746DE4EC5A82/2018/fut/items/web/players_meta.json';
-    const PLAYER_IMAGE     = 'https://fifa17.content.easports.com/fifa/fltOnlineAssets/B1BA185F-AD7C-4128-8A64-746DE4EC5A82/2018/fut/items/images/players/html5/120x120/%s.png'; // Replaceable %s
+    const PLAYER_IMAGE     = 'https://www.easports.com/fifa/ultimate-team/web-app/content/B1BA185F-AD7C-4128-8A64-746DE4EC5A82/2018/fut/items/images/players/html5/120x120/%s.png'; // Replaceable %s
 }
